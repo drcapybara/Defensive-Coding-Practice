9e17e9bfb155f9396eb8619815d28e3e8e0b1eb4
Dustin Ray <dustiv2@uw.edu>
2021-08-04 04:18:18 -0700

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,11 @@
+public class Application {
+
+
+    /** Driver for program. */
+    public static void main(String[] theArgs) {
+
+        InputValidator inValid = new InputValidator();
+
+    }
+
+}
---
+++ src/InputValidator.java
@@ -0,0 +1,39 @@
+/**
+ * This program:
+ *      prompts for and reads the user's first name, then last name
+ *                  -- each should be at most 50 characters
+ *                  -- decide what is legitimate for characters for first and last name
+ *      prompts for and reads in two int values from the user (range of values are those of a 4 byte int)
+ *      prompts for and reads the name of an input file from the user
+ *      prompts for reads the name of an output file from the user
+ *      prompts the user to enter a password,
+ *                          store the password,
+ *                          then ask the user to re-enter the password and verify that it is correct
+ *      password should be hashed using a salt and written to a file
+ *      to validate, grab hash from file and compare it to hash from second user entry for password
+ *      opens the output file and
+ *          writes the user's name
+ *          writes the result of adding the two integer values (no overflow should occur)
+ *          writes the result of multiplying the two integer values (no overflow should occur),
+ *          writes the contents of the input file
+ *
+ *      Each thing written should be clearly labeled (e.g. First name,
+ *                                                    Last name,
+ *                                                    First Integer,
+ *                                                    Second Integer,
+ *                                                    Sum,
+ *                                                    Product,
+ *                                                    Input File Name,
+ *                                                    Input file contents)
+ *
+ * NOTE: it is ok to echo output to the screen as you wish
+ *
+ * @author Dustin Ray
+ * @version Summer 2021
+ *
+ */
+public class InputValidator {
+
+
+
+}
