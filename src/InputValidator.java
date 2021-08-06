1ae8ba0c046cf842ce1069af59a9b3b38e808e7a
Dustin Ray <dustiv2@uw.edu>
2021-08-05 17:52:07 -0700

    commit

--- src/InputValidator.java
+++ src/InputValidator.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * This program:
  *      prompts for and reads the user's first name, then last name
@@ -5,7 +10,7 @@
  *                  -- decide what is legitimate for characters for first and last name
  *      prompts for and reads in two int values from the user (range of values are those of a 4 byte int)
  *      prompts for and reads the name of an input file from the user
- *      prompts for reads the name of an output file from the user
+ *      prompts for and reads the name of an output file from the user
  *      prompts the user to enter a password,
  *                          store the password,
  *                          then ask the user to re-enter the password and verify that it is correct
@@ -34,8 +39,138 @@
  */
 public class InputValidator {
 
+    private String myFirstName;
+    private String myLastName;
+    private int myFirstInt;
+    private int mySecondInt;
+    private File myInputFile;
+    private File myOutputFile;
+    private String myPassword;
+
+
     public InputValidator() {
-        System.out.println("this is a test");
+
+        getFirstName();
+        getLastName();
+        getIntOne();
+        getIntTwo();
+    }
+
+
+    private void getFirstName() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your first name: ");
+        String regex = "^([a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð,'-]{2,50})$";
+        String input = sc.nextLine();
+
+        while (checkPattern(input, regex)) {
+            sc.nextLine();
+            System.out.println("Im being really generous with what's allowed here... Please enter a valid first name... ");
+        };
+        myFirstName = input;
+    }
+
+    private void getLastName() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your last name: ");
+        String regex = "^([a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð,'-]{2,50})$";
+        String input = sc.nextLine();
+
+        while (checkPattern(input, regex)) {
+            sc.nextLine();
+            System.out.println("You're breaking my heart... Please enter a valid last name... ");
+        };
+        myLastName = input;
+
+    }
+
+    private void getIntOne() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter an integer: ");
+
+        while (!sc.hasNextInt()) {
+            sc.nextLine();
+            System.out.println("But...I thought we were friends... Please enter a valid integer. ");
+        };
+        myFirstInt = sc.nextInt();
+    }
+
+    public void getIntTwo() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter another integer: ");
+        while (!sc.hasNextInt()) {
+            sc.nextLine();
+            System.out.println("Please enter a strin- wait I mean INTEGER... Please enter a valid integer...");
+        };
+        mySecondInt = sc.nextInt();
+    }
+
+
+    /**
+     * Prompts for and reads the name of an output file from the user. Requires file extension.
+     * Valid: c:\Test.txt | \\server\shared\Test.txt | \\server\shared\Test.t
+     * Invalid: c:\Test | \\server\shared | \\server\shared\Test.?
+     *
+     * https://regexlib.com/REDetails.aspx?regexp_id=425
+     * */
+    public void getInputFilePath() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a path to an input file: ");
+        String input = sc.nextLine();
+        String regex = "^([a-zA-Z]\\:|\\\\\\\\[^\\/\\\\:*?\"<>|]+\\\\[^\\/\\\\:*?\"<>|]+)(\\\\[^\\/\\\\:*?\"<>|]+)+(\\.[^\\/\\\\:*?\"<>|]+)$";
+
+        //first check to see if filepath is of valid format.
+        while (checkPattern(input, regex)) {
+            sc.nextLine();
+            System.out.println("No...Please...Stop...Please enter a valid path to an input file: ");
+        };
+
+        //now check to see if that file actually exists.
+        File inputFile = new File(input);
+        while (!inputFile.exists() || inputFile.isDirectory()) {
+            sc.nextLine();
+            System.out.println("If you're trying to break me you'll have to do better than that... :)");
+        }
+        myInputFile = inputFile;
+    }
+
+    private void getOutPutFile() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a filepath to the location you wish to save the output file to: ");
+        String input = sc.nextLine();
+        String regex = "^([a-zA-Z]\\:|\\\\\\\\[^\\/\\\\:*?\"<>|]+\\\\[^\\/\\\\:*?\"<>|]+)(\\\\[^\\/\\\\:*?\"<>|]+)+(\\.[^\\/\\\\:*?\"<>|]+)$";
+        //first check to see if filepath is of valid format.
+        while (checkPattern(input, regex)) {
+            sc.nextLine();
+            System.out.println("java.io.FileNotFoundException haha just kidding please enter a valid filepath to the output file: ");
+        };
+        //now check to see if that file actually exists.
+        File outputFile = new File(input);
+        while (!outputFile.exists() || outputFile.isDirectory()) {
+            sc.nextLine();
+            System.out.println("I spent all of that time making this nice output file for you and you just pull it out from under me like that...rude");
+        }
+        myOutputFile = outputFile;
+    }
+
+
+    /**
+     * RegEx pattern checker.
+     * @param theInputString is the string to match.
+     * @param theRegex is the regex to match against.
+     */
+    private boolean checkPattern(final String theInputString, final String theRegex){
+        Pattern pattern = Pattern.compile(theRegex);
+        Matcher matcher = pattern.matcher(theInputString);
+        boolean matches = matcher.matches();
+        System.out.println("Matches RegEx test: " + matches);
+        return !matches;
     }
 
 }
